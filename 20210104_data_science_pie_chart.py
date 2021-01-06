fe95d9d56ebcc9bfa4d5c12a9689677c36ba2650
Gilles Pilon <gillespilon13@gmail.com>
2021-01-05 20:06:16 -0600

    Refactor

--- 20210104_data_science_pie_chart.py
+++ 20210104_data_science_pie_chart.py
@@ -5,7 +5,7 @@ Data science pie chart
 """
 
 from matplotlib import pyplot as plt
-import datasense as ds
+
 
 def main():
     labels = ['cleaning data', 'complaining about\ncleaning data']
@@ -15,12 +15,23 @@ def main():
     plt.xkcd()
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111)
-    ax.pie(sizes, labels=labels, colors=colours, autopct='%1.f %%')
+    ax.pie(
+        x=sizes,
+        labels=labels,
+        colors=colours,
+        autopct='%1.f %%'
+    )
     ax.set_title(label='data science')
     ax.text(
-        x=2, y=-1.25, s='gilles pilon 2021-01-04', horizontalalignment='right'
+        x=2,
+        y=-1.25,
+        s='gilles pilon 2021-01-04',
+        horizontalalignment='right'
     )
-    fig.savefig(fname='20210104_data_science_pie_chart.svg', format='svg')
+    fig.savefig(
+        fname='20210104_data_science_pie_chart.svg',
+        format='svg'
+    )
 
 
 if __name__ == '__main__':
